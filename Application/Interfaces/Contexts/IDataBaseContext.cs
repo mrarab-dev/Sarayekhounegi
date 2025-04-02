836f0081690a7eacd1d8f1e5ee81df95eac58d26
mohammadreza <mohammad79reza.arab@gmail.com>
2025-04-02 15:28:15 +0330

    Add-Migratio9n

---
+++ Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+        public DbSet<User> Users { get; set; }
+        int SaveChanges();
+       Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Contexts;
+using Domain.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class DataBaseContext : DbContext,IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions options):base(options) { }
+        public DbSet<User> Users { get; set; }
+       
+        
+    }
+}
